059d754f3c5cb96369d2cd304f4d6cb98e80e482
hira <ohno@toreta.in>
2020-05-04 16:28:49 +0900

    Initialize typescript

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import Head from 'next/head';
+
+const Home: FC = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+    </Head>
+    <div>Home</div>
+  </div>
+);
+
+export default Home;
